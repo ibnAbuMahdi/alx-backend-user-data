6c994e7eb646c9cfd29493a99156719c373a424a
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-08-03 16:22:43 +0000

    for all

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" encrypt password """
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """ returns salted, hashed password """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check whether hashed_password matches password """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
