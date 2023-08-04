386c481e4cfd5dbbfa6b854ad9b4d1ae01f4e443
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-08-04 12:57:53 +0000

    crxn 5 for encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """ returns salted, hashed password as byte string"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
