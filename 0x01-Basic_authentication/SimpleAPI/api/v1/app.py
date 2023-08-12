4c99be43ee6222c2ccbb22b52b877bfc543f2e0b
ibnAbuMahdi <rtkinspired@gmail.com>
2023-08-12 14:17:34 +0100

    pycodestyled the all files

--- 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/app.py
@@ -14,6 +14,7 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
--- 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ basic_auth """
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import (Tuple, TypeVar)
 import base64
 import binascii
 
@@ -15,7 +17,6 @@ class BasicAuth(Auth):
             return None
         return a_header.split()[1]
 
-
     def decode_base64_authorization_header(self, b64_header: str) -> str:
         """ return the decoded @b64_header if valid b64 string """
         if not b64_header or not isinstance(b64_header, str):
@@ -25,3 +26,37 @@ class BasicAuth(Auth):
         except (binascii.Error, ValueError) as e:
             return None
 
+    def extract_user_credentials(self, d_b64_header: str) -> Tuple(str):
+        """ return the credentials attached to the header
+            authorization values
+        """
+        if not d_b64_header or not isinstance(d_b64_header, str) or \
+                ':' not in d_b64_header:
+            return None
+        return d_b64_header.split(':')[0], \
+            d_b64_header[d_b64_header.find(':')+1:]
+
+    def user_object_from_credentials(self, email: str, pwd: str) \
+            -> TypeVar('User'):
+        """ create and return a User object from email and pwd """
+        if not email or not isinstance(email, str) \
+                or not pwd or not isinstance(pwd, str):
+            return None
+        users = User.search({'email': email})
+        if not len(users) or not users[0].is_valid_password(pwd):
+            return None
+        return users[0]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return the User instance based on Auth """
+        a_header = self.authorization_header(request=request)
+        if a_header:
+            b64_header = \
+                self.extract_base64_authorization_header(a_header=a_header)
+            if b64_header:
+                d_b64_header = \
+                    self.decode_base64_authorization_header(b64_header)
+                if d_b64_header:
+                    user_creds = self.extract_user_credentials(d_b64_header)
+                    if user_creds:
+                        return self.user_object_from_credentials(user_creds)
