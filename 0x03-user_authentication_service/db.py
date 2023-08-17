c784059b4a3335ab7d5f69307d24ba7f04685288
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-08-17 14:23:12 +0000

    crxn 6 for 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -19,8 +19,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False,
-                                     connect_args={"check_same_thread": False})
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
