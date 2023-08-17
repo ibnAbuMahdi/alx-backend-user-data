383edb750d872baece8e99d3802a17232dbedfb5
ibnAbuMahdi <r.muhammad@fubk.edu.ng>
2023-08-17 13:12:44 +0000

    reconfiged db engine

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,8 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False,
-                                     connect_args={"check_same_thread": False})
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
